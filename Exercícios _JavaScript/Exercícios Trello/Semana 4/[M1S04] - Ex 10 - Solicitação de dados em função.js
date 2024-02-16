840d9cabde158feb2a7aaf092b4bc8f9b4f901e7
nelsoncmjr <nelsoncmjr@gmail.com>
2024-02-16 17:20:44 -0300

    Done

--- Exercícios _JavaScript/Exercícios Trello/Semana 4/[M1S04] - Ex 10 - Solicitação de dados em função.js
+++ Exercícios _JavaScript/Exercícios Trello/Semana 4/[M1S04] - Ex 10 - Solicitação de dados em função.js
@@ -1,3 +1,16 @@
-// Crie uma função que solicite para o usuário através do prompt de comando sua idade, 
-// e a partir da idade fornecida, exiba uma mensagem informando se ele é maior de idade ou não. 
+// Crie uma função que solicite para o usuário através do prompt de comando sua idade,
+// e a partir da idade fornecida, exiba uma mensagem informando se ele é maior de idade ou não.
 // Considere 18 anos como maior idade..
+
+let idade = window.prompt("Digite sua idade");
+let idadeInt = Number(idade);
+
+function idadeFuncao(menorMaior) {
+  if (menorMaior >= 18) {
+    console.log("Você é maior de idade");
+  } else {
+    console.log("Você é menor de idade");
+  }
+}
+
+idadeFuncao(idadeInt);
